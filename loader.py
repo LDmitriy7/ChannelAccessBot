6ea0512576e7def4b42e2b8d686ddca61206f2ff
levch <levchenko.d.a1998@gmail.com>
2022-01-29 19:28:10 +0300

    v 0.1.1

--- loader.py
+++ loader.py
@@ -1,31 +1,29 @@
 import logging
 
-import mongoengine as me
 from aiogram import Bot, Dispatcher, types
-from aiogram.contrib.fsm_storage.mongo import MongoStorage
 
 import config
 from api import Wallet
 
-me.connect(
-    db=config.Database.name,
-    username=config.Database.username,
-    password=config.Database.password,
-    host=config.Database.host,
-    port=config.Database.port,
-    authentication_source=config.Database.auth_source,
-)
-
-storage = MongoStorage(
-    db_name=config.Database.name,
-    username=config.Database.username,
-    password=config.Database.password,
-    host=config.Database.host,
-    port=config.Database.port,
-)
+# me.connect(
+#     db=config.Database.name,
+#     username=config.Database.username,
+#     password=config.Database.password,
+#     host=config.Database.host,
+#     port=config.Database.port,
+#     authentication_source=config.Database.auth_source,
+# )
+#
+# storage = MongoStorage(
+#     db_name=config.Database.name,
+#     username=config.Database.username,
+#     password=config.Database.password,
+#     host=config.Database.host,
+#     port=config.Database.port,
+# )
 
 bot = Bot(config.Bot.token, parse_mode=types.ParseMode.HTML)
-dp = Dispatcher(bot, storage=storage)
+dp = Dispatcher(bot)
 
 logging.basicConfig(filename=config.Log.file, level=config.Log.level)
 
